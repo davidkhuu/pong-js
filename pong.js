4c76d4b6bc5c9cb967a2191416b1e5e7218cd829
David Khuu <davidkhuu.dk@gmail.com>
2016-06-01 19:14:18 -0600

    Make game horizontal, add html file to run on
    Flipped pong from vertical to horizontal like the original. I also
    added the .html file for the game to run in.

--- pong.js
+++ pong.js
@@ -1,3 +1,10 @@
+// Constants
+var GAME_WIDTH = 600;
+var GAME_HEIGHT = 400;
+
+var KEY_UP = 38;
+var KEY_DOWN = 40;
+
 var animate = window.requestAnimationFrame ||
   window.webkitRequestAnimationFrame ||
   window.mozRequestAnimationFrame ||
@@ -5,10 +12,8 @@ var animate = window.requestAnimationFrame ||
 
 // Set up a 2D canvas
 var canvas = document.createElement('canvas');
-var width = 400;
-var height = 600;
-canvas.width = width;
-canvas.height = height;
+canvas.width = GAME_WIDTH;
+canvas.height = GAME_HEIGHT;
 var context = canvas.getContext('2d');
 
 // When the page loads, attach the canvas to the screen
@@ -33,10 +38,10 @@ Player.prototype.update = function() {
   for (var key in keysDown) {
     var value = Number(key);
 
-    if (value == 37) { // left arrow
-      this.paddle.move(-4, 0);
-    } else if (value == 39) { // right arrow
-      this.paddle.move(4, 0);
+    if (value == KEY_UP) { // up arrow
+      this.paddle.move(0, -4);
+    } else if (value == KEY_DOWN) { // down arrow
+      this.paddle.move(0, 4);
     } else {
       this.paddle.move(0, 0);
     }
@@ -44,20 +49,20 @@ Player.prototype.update = function() {
 }
 
 Computer.prototype.update = function(ball) {
-  var x_pos = ball.x;
-  var diff = -( (this.paddle.x + (this.paddle.width / 2) ) - x_pos);
+  var y_pos = ball.y;
+  var diff = -( (this.paddle.y + (this.paddle.height / 2) ) - y_pos);
   if (diff < 0  &&  diff < -4) { // max speed left
     diff = -4;
   } else if (diff > 0 && diff > 4) { // max speed right
     diff = 4;
   }
 
-  this.paddle.move(diff, 0);
+  this.paddle.move(0, diff);
 
-  if (this.paddle.x < 0) {
-    this.paddle.x = 0;
-  } else if (this.paddle.x + this.paddle.width > 400) {
-    this.paddle.x = 400 - this.paddle.width;
+  if (this.paddle.y < 0) {
+    this.paddle.y = 0;
+  } else if (this.paddle.y + this.paddle.height > 400) {
+    this.paddle.y = 400 - this.paddle.height;
   }
 }
 
@@ -66,24 +71,24 @@ Paddle.prototype.move = function(x, y) {
   this.y += y;
   this.x_speed = x;
   this.y_speed = y;
-  if (this.x < 0) {
-    // all the way to the left
-    this.x = 0;
-    this.x_speed = 0;
-  } else if (this.x + this.width > 400) {
-    // all the way to the right
-    this.x = 400 - this.width;
-    this.x_speed = 0;
+  if (this.y < 0) {
+    // all the way to the bottom
+    this.y = 0;
+    this.y_speed = 0;
+  } else if (this.y + this.height > GAME_HEIGHT) {
+    // all the way to the top
+    this.y = GAME_HEIGHT - this.height;
+    this.y_speed = 0;
   }
 }
 
 var player = new Player();
 var computer = new Computer();
-var ball = new Ball(200, 300);
+var ball = new Ball(GAME_WIDTH/2, GAME_HEIGHT/2);
 
 var render = function() {
   context.fillStyle = "#000000";
-  context.fillRect(0, 0, width, height);
+  context.fillRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
 
   player.render();
   computer.render();
@@ -106,11 +111,11 @@ Paddle.prototype.render = function() {
 }
 
 function Player() {
-  this.paddle = new Paddle(175, 580, 50, 10);
+  this.paddle = new Paddle(10, 175, 10, 50);
 }
 
 function Computer() {
-  this.paddle = new Paddle(175, 10, 50, 10);
+  this.paddle = new Paddle(GAME_WIDTH-20, 175, 10, 50);
 }
 
 Player.prototype.render = function() {
@@ -124,8 +129,8 @@ Computer.prototype.render = function() {
 function Ball(x, y) {
   this.x = x;
   this.y = y;
-  this.x_speed = 0;
-  this.y_speed = 3;
+  this.x_speed = 3;
+  this.y_speed = 0;
   this.radius = 5;
 }
 
@@ -146,36 +151,48 @@ Ball.prototype.update = function(paddle1, paddle2) {
   var bottom_x = this.x + 5;
   var bottom_y = this.y + 5;
 
-  if (this.x - 5 < 0) { // hitting the left wall
-    this.x = 5;
-    this.x_speed = -this.x_speed;
-  } else if (this.x + 5 > 400) { // hitting the right wall
-    this.x = 395;
-    this.x_speed = -this.x_speed;
+  if (this.y - 5 < 0) { // hitting the top wall
+    this.y = 5;
+    this.y_speed = -this.y_speed;
+  } else if (this.y + 5 > GAME_HEIGHT) { // hitting the bottom wall
+    this.y = GAME_HEIGHT-5;
+    this.y_speed = -this.y_speed;
   }
 
-  if (this.y < 0 || this.y > 600) { // a point was scored
-    this.x_speed = 0;
-    this.y_speed = 3;
-    this.x = 200; // reset the ball
-    this.y = 300;
+  if (this.x < 0 || this.x > GAME_WIDTH) { // a point was scored
+    this.x_speed = 3;
+    this.y_speed = 0;
+    this.x = GAME_WIDTH/2; // reset the ball
+    this.y = GAME_HEIGHT/2;
   }
 
-  if (top_y > 300) {
+  if (top_x < 300) {
     if (top_y < (paddle1.y + paddle1.height) && bottom_y > paddle1.y
       && top_x < (paddle1.x + paddle1.width) && bottom_x > paddle1.x) {
       // hit the player's paddle
+      this.x_speed = 3;
+      this.y_speed += (paddle1.y_speed / 2);
+      this.x += this.x_speed;
+      /*
       this.y_speed = -3;
       this.x_speed += (paddle1.x_speed / 2);
       this.y += this.y_speed;
+      */
     }
   } else {
     if (top_y < (paddle2.y + paddle2.height) && bottom_y > paddle2.y
       && top_x < (paddle2.x + paddle2.width) && bottom_x > paddle2.x) {
       // hit the computer's paddle
+
+      this.x_speed = -3;
+      this.y_speed += (paddle2.y_speed / 2);
+      this.x += this.x_speed;
+
+      /*
       this.y_speed = 3;
       this.x_speed += (paddle2.x_speed / 2);
       this.y += this.y_speed;
+      */
     }
   }
 };
